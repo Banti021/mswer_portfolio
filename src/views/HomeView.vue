3e35a13d676626b35ff0a6ecc6be6427891d3b39
Banti021 <igor.lasota21@gmail.com>
2024-10-28 19:17:40 +0100

    Adjusted navbar + spline header

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -38,7 +38,32 @@ const projects = {
 
 <template>
   <nav class="sticky top-0 flex py-4 px-4 md:px-10 bg-black">
-    <img src="@/assets/logo.svg" class="max-h-[40px] md:max-h-[50px]" alt="Logo Ms.Wer">
+    <div class="w-full flex justify-between items-center gap-4">
+      <img src="@/assets/logo.svg" class="min-h-[40px] max-h-[40px] md:max-h-[50px]" alt="Logo Ms.Wer">
+      <div class="flex justify-evenly items-center gap-5">
+        <a class="text-white" href="https://www.instagram.com/mswer.art/profilecard/?igsh=MWkzdW10bGVkYzU3dA=="
+           target="_blank">
+          <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 448 512"
+              class="w-6 h-6 fill-white hover:fill-gray-300"
+          >
+            <!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+            <path
+                d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"/>
+          </svg>
+        </a>
+        <a href="tel:793 020 217">
+          <svg xmlns="http://www.w3.org/2000/svg"
+               viewBox="0 0 448 512"
+               class="w-5 h-5 fill-white hover:fill-gray-300">
+            <!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+            <path
+                d="M164.9 24.6c-7.7-18.6-28-28.5-47.4-23.2l-88 24C12.1 30.2 0 46 0 64C0 311.4 200.6 512 448 512c18 0 33.8-12.1 38.6-29.5l24-88c5.3-19.4-4.6-39.7-23.2-47.4l-96-40c-16.3-6.8-35.2-2.1-46.3 11.6L304.7 368C234.3 334.7 177.3 277.7 144 207.3L193.3 167c13.7-11.2 18.4-30 11.6-46.3l-40-96z"/>
+          </svg>
+        </a>
+      </div>
+    </div>
   </nav>
   <div class="bg-gray-200">
     <!-- Spline animation -->
@@ -133,7 +158,7 @@ const projects = {
   <!-- Contact -->
   <div class="flex flex-col justify-center items-center py-12 md:py-20 px-4 md:px-0">
     <p class="text-4xl md:text-5xl text-white font-bold text-center mb-4">Contact me</p>
-    <a class="text-white text-lg md:text-xl" href="mailto:werapecak@gmail.com">werapecak@gmail.com</a>
+    <a class="text-white text-lg md:text-xl" href="mailto:mswer.art@gmail.com">mswer.art@gmail.com</a>
   </div>
 </template>
 
