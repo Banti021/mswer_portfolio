3dc239d28dd2aac170a11c3d9f383af92a389e57
Banti021 <igor.lasota21@gmail.com>
2024-10-28 18:20:53 +0100

    Added favicon and website title

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,6 @@ import HomeView from "@/views/HomeView.vue";
 <template>
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Inspiration&display=swap" rel="stylesheet">
   <div class="bg-black">
     <HomeView/>
   </div>
